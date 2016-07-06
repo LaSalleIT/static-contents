d9b9f763fa3e1df454c057bb5101073de7208bc6
Alex Fang <frjalex@gmail.com>
2016-07-06 17:34:44 +0000

    Add custom js

--- rc1/rc1.js
+++ rc1/rc1.js
@@ -26145,3 +26145,281 @@ return Outlayer;
 
 }));
 
+
+        /**
+         *  LSTweets renderer Vue frontend
+         * Formulates JSON data from the renderer and
+         * displays in HTML
+         * @author Alex Fang
+         * @email alex [at] lschs.org
+         * @date 6/8/2016
+         */
+        var counter = 1;
+        $(window).scroll(function () {
+            if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+                $('.hidden-binding-group-1').attr('style', 'display: block;');
+                counter++;
+            }
+        });
+        var query = '//lscode.lschs.org/sma/rest/' + $('#query-settings').attr('class');
+
+        //Load Vue devtools
+        //that we'll never ever use,
+        //but just in case...
+        Vue.config.devtools = true;
+
+        //XHR requests + Vue rendering
+        var handleResponse = function (status, response) {
+                var onePageCount = 25;
+                var tweets = JSON.parse(response);
+                var finalResult = [];
+                console.log(tweets);
+                //Link detection
+
+                for (var i = 0; i < tweets.length; i++) {
+                    if (tweets[i].text) {
+                        if (tweets[i].text.length > 120) {
+                            tweets[i].shortenedText = tweets[i].text.substring(0, 110) + "...";
+                        } else {
+                            tweets[i].shortenedText = tweets[i].text;
+                        }
+
+                        tweets[i].text = tweets[i].text.autoLink();
+                        tweets[i].text = TwitterText.auto_link(tweets[i].text);
+
+                        tweets[i].shortenedText = tweets[i].shortenedText.autoLink();
+                        tweets[i].shortenedText = TwitterText.auto_link(tweets[i].shortenedText);
+                    } else {
+                        tweets[i].text = ' ';
+                    }
+                    tweets[i].count = i;
+                }
+
+                if (tweets.length > onePageCount) {
+                    //pagintation strategy
+                    for (var i = 0; i < Math.floor(tweets.length / onePageCount); i++) {
+                        var temp = [];
+                        var counter = 0;
+                        for (var j = (i * onePageCount); j < ((i + 1) * onePageCount); j++) {
+                            temp[counter] = tweets[j];
+                            counter++;
+                        }
+                        finalResult[i] = temp;
+                    }
+                } else {
+                    //only one page
+                    finalResult[0] = tweets;
+                }
+
+                var originalText;
+                if (finalResult.length > 1) {
+                    originalText = '<div class="grid-sizer"></div>\
+                    <div class="grid-item" id="vue-js-binding" v-for="tweet in tweets"> \
+            <div class="panel-body"> \
+                <p class="soft-wrapping">{{{ tweet.shortenedText }}}</p> \
+                <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                    <img src="{{ tweet.mediaurl }} " class="img-responsive" alt=""/>  \
+                </div> \
+                <br> \
+            </div> \
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#{{ tweet.count }}"> \
+                View details \
+            </button> \
+                        <p align="right">👍🏼 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                        💬 {{ tweet.comments }}</p> \
+        <div class="modal fade" id="{{ tweet.count }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
+            <div class="modal-dialog" role="document"> \
+                <div class="modal-content"> \
+                    <div class="modal-header"> \
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"> \
+                            <span aria-hidden="true">&times;</span> \
+                        </button> \
+                        <h4 class="modal-title" id="myModalLabel">{{{ tweet.source }}}</h4> \
+                    </div> \
+                    <div class="modal-body"> \
+                        {{{ tweet.text }}} \
+                        <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                            <img src="{{ tweet.mediaurl }}" alt="" class="img-thumbnail"/> \
+                        </div> \
+                        <hr> \
+                        <p>👍🏼 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                        💬 {{ tweet.comments }}</p> \
+                        <a href="{{ tweet.url }}">Original Post</a> \
+                    </div> \
+                    <div class="modal-footer"> \
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> \
+                    </div> \
+                </div> \
+            </div> \
+        </div> \
+    </div> \
+                ';
+                    for (var k = 1; k < finalResult.length; k++) {
+                        originalText = originalText + ' <div class="grid-item hidden-binding-group-' + k + '" id="vue-js-binding-' + k + '" v-for="tweet in tweets" \
+         style="display: none;"> \
+            <div class="panel-body"> \
+                <p class="soft-wrapping">{{{ tweet.shortenedText }}}</p> \
+                <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                    <img src="{{ tweet.mediaurl }} " class="img-responsive" alt=""/> \
+                </div> \
+                <br> \
+            </div> \
+            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#{{ tweet.count }}"> \
+                View details \
+            </button> \
+                        <p align="right">👍🏼 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                        💬 {{ tweet.comments }}</p> \
+        <div class="modal fade" id="{{ tweet.count }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
+            <div class="modal-dialog" role="document"> \
+                <div class="modal-content"> \
+                    <div class="modal-header"> \
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"> \
+                            <span aria-hidden="true">&times;</span> \
+                        </button> \
+                        <h4 class="modal-title" id="myModalLabel">{{{ tweet.source }}}</h4> \
+                    </div> \
+                    <div class="modal-body"> \
+                        {{{ tweet.text }}} \
+                        <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                            <img src="{{ tweet.mediaurl }}" alt="" class="img-thumbnail"/> \
+                        </div> \
+                        <hr> \
+                        <p>👍 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                        💬 {{ tweet.comments }}</p> \
+                        <a href="{{ tweet.url }}">Original Post</a> \
+                    </div> \
+                    <div class="modal-footer"> \
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> \
+                    </div> \
+                </div> \
+            </div> \
+        </div> \
+    </div> \
+        ';
+                    }
+                    originalText = originalText + '' +
+                        '</div>';
+                    document.getElementById("sma-widget-container").innerHTML = originalText;
+
+
+                    tweets = finalResult[0]; //render 0
+                    var twt = [];
+                    twt[1] = new Vue({
+                        el: "#vue-js-binding",
+                        data: {
+                            tweets
+                        }
+                    });
+                    for (var l = 1; l < finalResult.length; l++) { //begin from 2nd
+                        tweets = finalResult[l];
+                        twt[l] = new Vue({
+                            el: "#vue-js-binding-" + l,
+                            data: {
+                                tweets
+                            }
+                        });
+                    }
+                }
+                else { //not
+
+                    document.getElementById("sma-widget-container").innerHTML = ' <div class="grid-item" id="vue-js-binding" v-for="tweet in tweets"> \
+        <div class="panel-body"> \
+            <p class="soft-wrapping">{{{ tweet.shortenedText }}}</p> \
+            <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                <img src="{{ tweet.mediaurl }} " class="img-responsive" alt=""/> \
+            </div> \
+            <br> \
+        </div> \
+        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#{{ tweet.count }}"> \
+            View details \
+        </button> \
+                        <p align="right">👍🏼 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                        💬 {{ tweet.comments }}</p> \
+    <div class="modal fade" id="{{ tweet.count }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
+        <div class="modal-dialog" role="document"> \
+            <div class="modal-content"> \
+                <div class="modal-header"> \
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"> \
+                        <span aria-hidden="true">&times;</span> \
+                    </button> \
+                    <h4 class="modal-title" id="myModalLabel">{{{ tweet.source }}}</h4> \
+                </div> \
+                <div class="modal-body"> \
+                    {{{ tweet.text }}} \
+                    <div id="inline-media" v-if=" tweet.ifmediaexists "> \
+                        <img src="{{ tweet.mediaurl }}" alt="" class="img-thumbnail"/> \
+                    </div> \
+                    <hr> \
+                    <p>👍 {{ tweet.likes }} &nbsp;&nbsp;&nbsp; \
+                    💬 {{ tweet.comments }}</p> \
+                    <a href="{{ tweet.url }}">Original Post</a> \
+                </div> \
+                <div class="modal-footer"> \
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> \
+                </div> \
+            </div> \
+        </div> \
+    </div> \
+</div> \
+';
+
+                    tweets = finalResult[0];
+                    var twtBoxRender = new Vue({
+                        el: "#vue-js-binding",
+                        data: {
+                            tweets
+                        }
+                    });
+
+                    $(window).scroll(function () {
+                        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+                            $('.hidden-binding-group-1').attr('style', 'display: block;');
+                            counter++;
+                        }
+                    });
+                    $(window).scroll(function () {
+                        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+                            $('.hidden-binding-group-1').attr('style', 'display: block;');
+                            counter++;
+                        }
+                    });
+
+                    console.log('Here before loading masonry');
+                    //Masonry jQuery plugin
+                    $('.grid').masonry({
+                      // options
+                      itemSelector: '.grid-item',
+                      // use element for option
+                      columnWidth: '.grid-sizer',
+                      percentPosition: true
+                    });
+                    console.log('Here after loading masonry');
+                }
+            }
+            ;
+
+
+        //
+
+        var handleStateChange = function () {
+            switch (xmlhttp.readyState) {
+                case 0 : // UNINITIALIZED
+                case 1 : // LOADING
+                case 2 : // LOADED
+                case 3 : // INTERACTIVE
+                    break;
+                case 4 : // COMPLETED
+                    handleResponse(xmlhttp.status, xmlhttp.responseText);
+                    break;
+                default:
+                    alert("error");
+            }
+        };
+
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = handleStateChange;
+        xmlhttp.open("GET", query, true);
+        xmlhttp.send(null);
+
+        /* End of async call, no code shall be added here */
+
