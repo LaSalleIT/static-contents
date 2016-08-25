619054d95913b3b7b28c28c2ebb3208f06fb2e69
Alex Fang <frjalex@temple.edu>
2016-08-25 14:33:49 -0400

    Update test-sma-inject.js

--- rc3/test-sma-inject.js
+++ rc3/test-sma-inject.js
@@ -91,13 +91,13 @@
 		 </div> \
         <div class="modal fade" id="{{ tweet.count }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
             <div class="modal-dialog" role="document"> \
-               <div class="modal-content"> \
+               <div> \
                 <div style="max-width: 50%;" class="align-left" id="horizontal-resize"> \
                   <div id="inline-media" v-if=" tweet.ifmediaexists "> \
                         <img class="align-left" src="{{ tweet.mediaurl }}" alt="" class="img-responsive"/> \
                   </div> \
                 </div> \
-                <div style="max-width: 50%;" id="horizontal-resize" class="align-right" style="background: white;"> \
+                <div style="max-width: 50%;" id="horizontal-resize" class="align-right modal-content" style="background: white;"> \
                     <div class="modal-header"> \
                         <i class="fa fa-times-circle-o fa-2x" data-dismiss="modal" aria-label="Close"> \
                         </i> \
