fb0a9c9498386cb2cf55a366eaa41a0c17fe160c
Alex Fang <frjalex@gmail.com>
2016-08-25 15:11:07 -0400

    Fix not showing white background problem in load more

--- rc3/test-sma-inject.js
+++ rc3/test-sma-inject.js
@@ -141,6 +141,7 @@
 	</div> \
         <div class="modal fade" id="{{ tweet.count }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"> \
             <div class="modal-dialog" role="document"> \
+               <div class="modal-content"> \
                 	<div class="modal-body"> \
                 		<div style="max-width: 50%;" id="horizontal-resize"> \
                     			<div id="inline-media" v-if=" tweet.ifmediaexists "> \
